1670838f4cf782be73cc7051f96a56c5198ad4bb
iau <iau@PC-425>
2019-01-28 09:26:52 +0300

    Kisi class eklendi

---
+++ DataAnnotatlonApp/Entity/Firma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotatlonApp.Entity
+{
+    [Table("Firma", Schema = "dbo")]
+
+    public class Firma
+    {
+        [Key]
+        [Column("FirmaId",Order =0)]
+        public Guid Id { get; set; }
+        [Column("FirmaUnvani",Order =1,TypeName ="ntext")]
+        public string Unvan { get; set; }
+
+        public string Adres { get; set; }
+        [Column("TelefonNo",Order =3)]
+        public string Tel { get; set; }
+    }
+}
---
+++ DataAnnotatlonApp/Entity/Kisiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotatlonApp.Entity
+{
+    [Table("Kisiler",Schema ="dbo")]
+   public  class Kisiler
+    {
+        [Key]
+        [Column("KisiId",Order =0)]
+        public Guid KisiId { get; set; }
+        public Guid FirmaRefId { get; set; }
+        [Column("KisiAdi")]
+        [Required] // Girilmesi zorunlu olan alan
+        [StringLength(40)]
+        public string KisiAdi { get; set; }
+
+    }
+}
